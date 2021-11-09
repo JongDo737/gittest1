1a4c12e28d17defc3a2a3532b36f72eb34b444d2
JongDo737 <jongmin373@gmail.com>
2021-11-09 17:40:16 +0900

    래퍼클래스 학습

---
+++ chapter06/src/ch05/Wrapper_Ex2.java
@@ -0,0 +1,13 @@
+package ch05;
+
+public class Wrapper_Ex2 {
+		
+	public static void main(String[] args) {
+		
+//		Interger 형 <- int 형
+		Integer num = 17;		//오토 박싱
+		int n = num;				//자동 언박싱
+		System.out.println(n);
+		
+	}
+}
---
+++ chapter06/src/ch05/Wrapper_Ex3.java
@@ -0,0 +1,48 @@
+package ch05;
+
+public class Wrapper_Ex3 {
+
+	public static void main(String[] args) {
+
+		String str = "10";
+		String str2 = "11.5";
+		String str3 = "true";
+
+		// 8 + "" --> String
+		byte b = Byte.parseByte(str);
+		System.out.println("문자열 byte값 변환 : " + b);
+
+		int i = Integer.parseInt(str);
+		System.out.println("문자열 int값 변환 : " + i);
+
+		short s = Short.parseShort(str);
+		System.out.println("문자열 short값 변환 : " + s);
+
+		long l = Long.parseLong(str);
+		System.out.println("문자열 long값 변환 : " + l);
+
+		float f = Float.parseFloat(str2);
+		System.out.println("문자열 float값 변환 : " + f);
+
+		double d = Double.parseDouble(str2);
+		System.out.println("문자열 double값 변환 : " + d);
+
+		boolean bool = Boolean.parseBoolean(str3);
+		System.out.println("문자열 boolean값 변환 : " + bool);
+
+		// 문제 1
+		String str10 = "1001";
+		String str11 = "2001";
+
+		// str10 과 str11을 덧셈 연산
+		// str10 과 str11을 곱셈 연산
+
+		int num1 = Integer.parseInt(str10) + Integer.parseInt(str11);
+		System.out.println(num1);
+
+		int num2 = Integer.parseInt(str10) * Integer.parseInt(str11);
+		System.out.println(num2);
+
+	}
+
+}
