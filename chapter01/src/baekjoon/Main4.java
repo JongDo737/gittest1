a797474e47ee628f59a1b16f6ba8ec8d5fadd50a
JongDo737 <jongmin373@gmail.com>
2021-12-14 13:44:22 +0900

    백준

---
+++ chapter01/src/baekjoon/Main4.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		int num = 1;
+		int count = 1;
+		while (num < n) {
+			num +=6 * count;	
+			count++;
+		}
+		System.out.println(count);
+	}
+
+}
